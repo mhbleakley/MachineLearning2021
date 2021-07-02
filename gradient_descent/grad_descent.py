8ce14e4300dbef4ab2650f4b940945a748c9117e
mhbleakley <mhbleakley@wpi.edu>
2021-07-02 11:36:22 -0400

    isolated prediction days

--- gradient_descent/grad_descent.py
+++ gradient_descent/grad_descent.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
 import numpy as np
-import pandas as pd
 
 style.use("dark_background")
 
@@ -11,11 +10,6 @@ y1 = np.array([98, 68, 81, 80, 83, 52, 66, 30, 68, 73])
 m = [0.0]
 b = [0.0]
 iteration = [0]
-
-
-# takes in lists
-def data_generator(x, y):
-    return np.array(x), np.array(y)
 
 
 def update_line(x, y):
@@ -35,33 +29,34 @@ def update_line(x, y):
     data.close()
 
 
-def animate_line(i, x, y):
-    update_line(x, y)
-    iteration.append(iteration[0] + 1)
-    iteration.pop(0)
-    data = open("mb.txt", "r")
-    m_curr = b_curr = 0
-    for line in data:
-        if len(line) > 0:
-            m_curr = float(line.split(",")[0])
-            b_curr = float(line.split(",")[1])
+def animate_line(i, x, y, it):
+    if iteration[0] < it:
+        update_line(x, y)
+        iteration.append(iteration[0] + 1)
+        iteration.pop(0)
+        data = open("mb.txt", "r")
+        m_curr = b_curr = 0
+        for line in data:
+            if len(line) > 0:
+                m_curr = float(line.split(",")[0])
+                b_curr = float(line.split(",")[1])
 
-    y_pred = (m_curr * max(x)) + b_curr
-    xs = [0, max(x) + (0.1 * max(x))]
-    ys = [b_curr, y_pred]
-    # print(m_curr)
-    # print(b_curr)
-    # print(y)
-    # print()
-    plt.clf()
-    plt.annotate("Iteration #" + str(iteration[0]), xy=(0.1 * min(x), 0.9 * max(y)))
-    plt.annotate(("m = " + str(round(m_curr, 4))), xy=(0.1 * min(x), 0.8 * max(y)))
-    plt.annotate(("b = " + str(round(b_curr, 4))), xy=(0.1 * min(x), 0.7 * max(y)))
-    plt.grid(color="#333", alpha=.5)
-    plt.plot(xs, ys, "--", color="#1b1")
-    plt.scatter(x, y, marker="o", linewidths=0.5, color="#42f2f5")
-    plt.tight_layout()
+        y_pred = (m_curr * max(x)) + b_curr
+        xs = [0, max(x) + (0.1 * max(x))]
+        ys = [b_curr, y_pred]
+        # print(m_curr)
+        # print(b_curr)
+        # print(y)
+        # print()
+        plt.clf()
+        plt.annotate("Iteration #" + str(iteration[0]), xy=(0.1 * min(x), 0.9 * max(y)))
+        plt.annotate(("m = " + str(round(m_curr, 4))), xy=(0.1 * min(x), 0.8 * max(y)))
+        plt.annotate(("b = " + str(round(b_curr, 4))), xy=(0.1 * min(x), 0.7 * max(y)))
+        plt.grid(color="#333", alpha=.5)
+        plt.plot(xs, ys, "--", color="#1b1")
+        plt.scatter(x, y, marker="o", linewidths=1, color="#42f2f5")
+        plt.tight_layout()
 
 
-ani = animation.FuncAnimation(plt.gcf(), animate_line, fargs=[x1, y1], interval=20)
+ani = animation.FuncAnimation(plt.gcf(), animate_line, fargs=[x1, y1, 200], interval=20)
 plt.show()
--- sp500/SP500.py
+++ sp500/SP500.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import yfinance as yf
 from matplotlib import style
@@ -8,10 +9,10 @@ reg = linear_model.LinearRegression()
 style.use("ggplot")  # style
 yf.pdr_override()  # yahoo finance override
 
-aapl = pd.read_csv("sp500/stock_dfs/AAPL.csv")
+aapl = pd.read_csv("stock_dfs/AAPL.csv")
 
 
-def prep_data(df, future=1):
+def prep_data(df, future=1, return_date=False):
     ndf = df.copy()
     ndf.drop(["Open", "High", "Low", "Close", "Volume"], 1, inplace=True)  # leaving date and adj close
     ndf = simple_moving_average(ndf, 5, False)  # column 2 (0 index)
@@ -21,33 +22,43 @@ def prep_data(df, future=1):
         ndf["D{} Close".format(-day)] = ndf["Adj Close"].shift(periods=day)
     ndf.drop(ndf.tail(future).index, inplace=True)
     ndf.fillna(0, inplace=True)
-    return ndf.iloc[:, 1:5], ndf.iloc[:, 5:]
-    # else:
-    #     ndf.fillna(0, inplace=True)
-    #     return ndf.iloc[:, 1:5], ndf.iloc[:, 5:]
-
-
-def isolate_result(prediction, actual, day):
-    adf = actual.copy()
-    pdf = prediction.copy()
-    new_df = pd.DataFrame()
-    new_df["Actual"] = adf["D{} Close".format(day)]
-    new_df["Predicted"] = pdf[day-1][:]
-    return new_df
+    if return_date:
+        return ndf.iloc[:, 0], ndf.iloc[:, 1:5], ndf.iloc[:, 5:]
+    else:
+        return ndf.iloc[:, 1:5], ndf.iloc[:, 5:]
 
 
 X_train, y_train = prep_data(aapl.iloc[:5000], 5)
-X_test, y_test = prep_data(aapl[5000:], 5)
+date, X_test, y_test = prep_data(aapl[5000:], 5, True)
 reg.fit(X_train, y_train)
 y_pred = reg.predict(X_test)
-print(y_pred)
-print(y_test)
-# print(X_test.index)
-print(isolate_result(y_pred, y_test, 4).head())
+predictions = pd.DataFrame().from_records(y_pred)
+predictions.transpose()
+predictions.columns = ["D{} Predicted".format(i) for i in range(1, 6)]
+y_test = y_test.shift(periods=5)
+# predictions = predictions.shift(periods=5)
+y_test = y_test.reset_index()
+y_test = y_test.join(predictions)
+results = pd.DataFrame()
+results["Date"] = date
+results = results.reset_index()
+results = results.drop(columns=["index"])
+results["D5 Actual"] = y_test["D5 Close"]
+results["D5 Pred."] = y_test["D5 Predicted"]
+results.drop(results.head(5).index, inplace=True)
+
+
+# print(results.tail(20))
+print(X_test.tail(10))
+print(y_test["D5 Close"].tail(10))
+print(predictions.tail(10))
+
+plt.plot(results["Date"], results["D5 Actual"])
+plt.plot(results["Date"], results["D5 Pred."])
 
 
 # plt.plot(aapl["Date"][5005:], aapl["Adj Close"][5005:])
 # plt.plot(aapl["Date"][5005:], y_test["D5 Close"])
 # plt.grid()
-# plt.tight_layout()
-# plt.show()
+plt.tight_layout()
+plt.show()
